2f191e2029dd5c269d3c4d1312a75b97f5e8666f
Colani Matsenjwa <matsenjwacolani@gmail.com>
2024-09-18 00:33:03 +0200

    css, js, images, SQLite DB setup & forms

--- hello.py
+++ hello.py
@@ -1,23 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 
 # Create a Flask Instance
 app = Flask(__name__)       # allows Flask to know where to look for templates and static files
 
-# Create a CSRS key to protect the form when submitting
+# Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Create a CSRF key to protect the form when submitting
 app.config['SECRET_KEY'] = "my super secret key for form submition"
+
+# Initialize the database
+db = SQLAlchemy(app)
+
+# Create a model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    # Create a string
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# Create a form class for the Users Model
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")    
 
 # Create a form class
 class NameForm(FlaskForm):
     name = StringField("What's Your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
-
-# Create a route decorator
-@app.route('/')
-
 
 #-FILTERS---------
 # safe
@@ -31,6 +52,8 @@ class NameForm(FlaskForm):
 # def index():
 #     return "<h1>Hello, World!</h1>"
 
+# Create a route decorator for index page
+@app.route('/')
 def index():
     first_name = "John"
     stuff = "This is <strong>Bold</strong> Text"
@@ -40,9 +63,30 @@ def index():
 
 # localhost:5000/user/Colani
 @app.route('/user/<name>')
-
 def user(name):
     return render_template("user.html",user_name=name)
+
+# Create a route decorator for UserForm
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    # Validate Form
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            # Add the user to the database
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data      # if form has data, assign it to name
+        form.name.data = ''
+        form.email.data = ''
+        flash("User Added Successfully!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", 
+                           form=form, name=name, 
+                           our_users=our_users)
 
 # Create Custom Error Pages
 # Invalid URL
@@ -64,6 +108,7 @@ def name():
     if form.validate_on_submit():
         name =  form.name.data      # if form has data, assign it to name
         form.name.data = ''         # clear the form once data is captured
+        flash("Form Submitted Successfully")
         
     return render_template("name.html",name = name, form = form)
 
